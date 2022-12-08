3338ed0561d791b8a79d055e49766bc2e98f1bca
avlebedev <avlebedev@fil-it.ru>
2022-12-08 14:33:07 +0300

    SecondTask restaraunt_ex commit

---
+++ src/main/scala/SecondTask.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object SecondTask extends App {
+
+  private val sparkSession: SparkSession = SparkSession
+    .builder()
+    .appName("SecondTask")
+    .master("local")
+    .getOrCreate()
+
+  private val dataFrame: DataFrame = sparkSession
+    .read
+    .format("json")
+    .option("inferSchema", "true")
+    .load("src/resources/restaurant_ex.json")
+
+  private val newDataFrame: DataFrame = dataFrame.withColumn("has_online_delivery", col("has_online_delivery").cast(IntegerType))
+    .withColumn("is_delivering_now", col("is_delivering_now").cast(IntegerType))
+
+  newDataFrame.printSchema()
+  sparkSession.close()
+
+}
