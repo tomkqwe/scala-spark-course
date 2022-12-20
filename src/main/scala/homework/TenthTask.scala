c095b4cb617a260c8a2978da7fc742db3d454fb8
avlebedev <avlebedev@fil-it.ru>
2022-12-20 13:35:01 +0300

    TenthTask dataframe work and i do some refactor

--- src/main/scala/homework/TenthTask.scala
+++ src/main/scala/homework/TenthTask.scala
@@ -1,7 +1,8 @@
 package homework
 
-import org.apache.spark.sql.functions.{col, lit, max, min, regexp_replace, sum}
-import org.apache.spark.sql.types.IntegerType
+import exercises.DataSetPracticePartTwo.ageDS.withColumnRenamed
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object TenthTask extends App {
@@ -13,77 +14,72 @@ object TenthTask extends App {
     .getOrCreate()
   spark.sparkContext.setLogLevel("OFF")
 
+  val schema = StructType(Seq(
+    StructField("JobTitle",StringType),
+    StructField("Company",StringType),
+    StructField("Location",StringType),
+    StructField("CompanyReviews",StringType),
+    StructField("Link",StringType)
+  ))
   val df = spark.read
-    .option("inferSchema", "true")
     .option("header", "true")
     .option("multiLine", "true")
+    .schema(schema)
     .csv("src/main/resources/AiJobsIndustry.csv")
 
-  import spark.implicits._
-
+  private val Company = "Company"
+  private val JobTitle = "JobTitle"
+  private val CompanyRewies = "CompanyReviews"
+  private val SumCompanyReviews = "sum(" + CompanyRewies + ")"
+  private val Location = "Location"
 
   val companyReview = editCompanyReviewsColumn
-    .withColumn("Company", regexp_replace(col("Company"), "[ *\\n *]", ""))
-    .groupBy("Company", "Location")
-    .sum("CompanyReviews")
-    .orderBy(col("sum(CompanyReviews)"))
+    .withColumn(Company, regexp_replace(col(Company), "[ *\\n *]", ""))
+    .groupBy(Company, Location)
+    .sum(CompanyRewies)
 
-  val minValueCompany = companyReview.select(min(col("sum(CompanyReviews)"))).collect().head.get(0)
-  val maxValueCompany = companyReview.select(max(col("sum(CompanyReviews)"))).collect().head.get(0)
-
-  private val minCompany: DataFrame = companyReview.where(col("sum(CompanyReviews)") === minValueCompany)
-    .withColumnRenamed("Company", "name")
-    .withColumnRenamed("Location", "location")
-    .withColumnRenamed("sum(CompanyReviews)", "count")
-    .withColumn("stats_type", lit("company"))
-    .withColumn("count_type", lit("min"))
-    .select("name", "stats_type", "location", "count", "count_type")
-
-  private val maxCompany:DataFrame = companyReview.where(col("sum(CompanyReviews)") === maxValueCompany)
-    .withColumnRenamed("Company", "name")
-    .withColumnRenamed("Location", "location")
-    .withColumnRenamed("sum(CompanyReviews)", "count")
-    .withColumn("stats_type", lit("company"))
-    .withColumn("count_type", lit("max"))
-    .select("name", "stats_type", "location", "count", "count_type")
-
+  val minValueCompany = companyReview.select(min(col(SumCompanyReviews))).collect().head.get(0)
+  val maxValueCompany = companyReview.select(max(col(SumCompanyReviews))).collect().head.get(0)
 
   val jobTitleReview = editCompanyReviewsColumn
-    .groupBy("JobTitle", "Location")
-    .sum("CompanyReviews")
-    .orderBy(col("sum(CompanyReviews)"))
+    .groupBy(JobTitle, Location)
+    .sum(CompanyRewies)
 
-  val minValueJobTitle = jobTitleReview.select(min(col("sum(CompanyReviews)"))).collect().head.get(0)
-  val maxValueJobTitle = jobTitleReview.select(max(col("sum(CompanyReviews)"))).collect().head.get(0)
+  val minValueJobTitle = jobTitleReview.select(min(col(SumCompanyReviews))).collect().head.get(0)
+  val maxValueJobTitle = jobTitleReview.select(max(col(SumCompanyReviews))).collect().head.get(0)
 
-   val minJobTitle: DataFrame = jobTitleReview.where(col("sum(CompanyReviews)") === minValueJobTitle)
-    .withColumnRenamed("JobTitle", "name")
-    .withColumnRenamed("Location", "location")
-    .withColumnRenamed("sum(CompanyReviews)", "count")
-    .withColumn("stats_type", lit("job"))
-    .withColumn("count_type", lit("min"))
-    .select("name", "stats_type", "location", "count", "count_type")
-  val maxJobTitle: DataFrame = jobTitleReview.where(col("sum(CompanyReviews)") === maxValueJobTitle)
-    .withColumnRenamed("JobTitle", "name")
-    .withColumnRenamed("Location", "location")
-    .withColumnRenamed("sum(CompanyReviews)", "count")
-    .withColumn("stats_type", lit("job"))
-    .withColumn("count_type", lit("max"))
-    .select("name", "stats_type", "location", "count", "count_type")
+   val maxCompany = getMaxAndMinReviews(maxValueCompany,Company)
+   val minCompany = getMaxAndMinReviews(minValueCompany,Company)
+   val minJobTitle = getMaxAndMinReviews(minValueJobTitle,JobTitle)
+   val maxJobTitle = getMaxAndMinReviews(maxValueJobTitle,JobTitle)
 
+  maxCompany.union(minCompany.union(minJobTitle.union(maxJobTitle)))
+    .select("name","stats_type","location","count","count_type")
+    .show()
 
-  maxCompany.union(maxJobTitle.union(minCompany.union(minJobTitle))).show()
-
-  //  companyReview
-  //    .show()
-  //  jobTitleReview
-  //    .show()
+  private def getMaxAndMinReviews(value:Any,companyOrJobTitle:String):DataFrame = {
+    val tmpDf = companyOrJobTitle match {
+      case TenthTask.Company => companyReview.where(col(SumCompanyReviews) === value).limit(1)
+        .withColumnRenamed(Company, "name")
+        .withColumn("stats_type", lit("company"))
+      case TenthTask.JobTitle => jobTitleReview.where(col(SumCompanyReviews) === value).limit(1)
+        .withColumnRenamed(JobTitle, "name")
+        .withColumn("stats_type", lit("job"))
+    }
+    val finalDf = tmpDf.withColumnRenamed(Location, "location")
+      .withColumnRenamed(SumCompanyReviews, "count")
+    value match{
+      case TenthTask.minValueCompany => finalDf.withColumn("count_type", lit("min"))
+      case TenthTask.maxValueCompany => finalDf.withColumn("count_type", lit("max"))
+      case TenthTask.minValueJobTitle => finalDf.withColumn("count_type",lit("min"))
+      case TenthTask.maxValueJobTitle => finalDf.withColumn("count_type",lit("max"))
+    }
+  }
 
   def editCompanyReviewsColumn: DataFrame = {
     df.na.drop()
-      .withColumn("CompanyReviews", regexp_replace(col("CompanyReviews"), "\\D", "").cast(IntegerType))
+      .withColumn(CompanyRewies, regexp_replace(col(CompanyRewies), "\\D", "").cast(IntegerType))
   }
 
-  //  private val str: String = "sas".replaceAll("[ *\\\\n *]", "")
   spark.close()
 }
